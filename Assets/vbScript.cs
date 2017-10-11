fdb9a188367d17106808f2e779041c6525d5bf0b
Nihar Chitnis <niharchitnis@gmail.com>
2017-10-11 23:46:35 +0530

    Augmented Reality

---
+++ Assets/vbScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vbScript : MonoBehaviour, IVirtualButtonEventHandler
+{
+    private GameObject vbButtonObject;
+    private GameObject zombie;
+    // Use this for initialization
+    void Start()
+    {
+        vbButtonObject = GameObject.Find("actionButton");
+        vbButtonObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        zombie = GameObject.Find("zombie");
+    }
+
+    public void OnButtonPressed(VirtualButtonAbstractBehaviour vb)
+    {
+        Debug.Log("button down!!!");
+        zombie.transform.Rotate(new Vector3(0, Time.deltaTime * 1000, 0));
+
+
+    }
+
+    public void OnButtonReleased(VirtualButtonAbstractBehaviour vb)
+    {
+
+    }
+}
